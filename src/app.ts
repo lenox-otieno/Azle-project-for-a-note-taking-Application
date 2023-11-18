2758ea0bb229694e1b326766640178fb0e1a89d2
eatahine <84812721+eatahine@users.noreply.github.com>
2023-11-18 19:11:18 +0100

    Update app.ts
    1. **Error Handling:**
       - Added try-catch blocks to handle potential errors more gracefully.
    
    2. **Validation:**
       - Implemented a more robust check for `noteId` to ensure it's a valid positive integer.
    
    3. **Middleware Configuration:**
       - Ensured that the `azle.json()` middleware is configured before the route handlers.
    
    4. **Security Considerations:**
       - While the use of `noteIdCounter` may be acceptable for small-scale applications, consider using a more sophisticated solution for generating unique IDs in a production environment.
    
    These adjustments enhance error handling and validation, providing a more reliable and secure implementation.

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@
 import * as azle from 'azle';
 
 const app = azle.createApp();
+
+// Middleware Configuration
 app.use(azle.json());
 
 interface Note {
@@ -14,39 +16,55 @@ let notes: Note[] = [];
 let noteIdCounter = 1;
 
 app.get('/notes', (req, res) => {
-  res.json(notes);
+  try {
+    res.json(notes);
+  } catch (error) {
+    handleError(res, error);
+  }
 });
 
 app.post('/notes', (req, res) => {
-  const { title, content } = req.body;
-  if (!title || !content) {
-    res.status(400).json({ error: 'Title and content are required' });
-    return;
+  try {
+    const { title, content } = req.body;
+
+    if (!title || !content) {
+      return res.status(400).json({ error: 'Title and content are required' });
+    }
+
+    const newNote: Note = {
+      id: noteIdCounter++,
+      title,
+      content,
+    };
+
+    notes.push(newNote);
+
+    res.status(201).json(newNote);
+  } catch (error) {
+    handleError(res, error);
   }
-
-  const newNote: Note = {
-    id: noteIdCounter++,
-    title,
-    content,
-  };
-
-  notes.push(newNote);
-
-  res.status(201).json(newNote);
 });
 
 app.delete('/notes/:id', (req, res) => {
-  const noteId = parseInt(req.params.id, 10);
-  const noteIndex = notes.findIndex((note) => note.id === noteId);
+  try {
+    const noteId = parseInt(req.params.id, 10);
 
-  if (noteIndex === -1) {
-    res.status(404).json({ error: 'Note not found' });
-    return;
+    if (isNaN(noteId) || noteId <= 0) {
+      return res.status(400).json({ error: 'Invalid note ID' });
+    }
+
+    const noteIndex = notes.findIndex((note) => note.id === noteId);
+
+    if (noteIndex === -1) {
+      return res.status(404).json({ error: 'Note not found' });
+    }
+
+    notes.splice(noteIndex, 1);
+
+    res.json({ message: 'Note deleted successfully' });
+  } catch (error) {
+    handleError(res, error);
   }
-
-  notes.splice(noteIndex, 1);
-
-  res.json({ message: 'Note deleted successfully' });
 });
 
 const PORT = process.env.PORT || 3000;
@@ -54,3 +72,10 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+// Helper function for handling errors
+function handleError(res: azle.Response, error: any) {
+  console.error(error);
+  res.status(500).json({ error: 'Internal Server Error' });
+  }
+        
